0159d55aaecefb6dee9f2600618aed1d1b9ec062
Muhammad Junaid <mjunaid.zafar01@gmail.com>
2026-01-13 23:22:04 +0500

    Update app.module.ts to load configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,11 +4,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { SessionsModule } from './sessions/sessions.module';
+import { configuration } from './config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      load: [configuration],
     }),
     DatabaseModule,
     SessionsModule,
